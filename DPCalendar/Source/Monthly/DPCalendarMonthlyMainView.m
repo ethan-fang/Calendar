2dc4f535d6d3770bf0425c7c9852028224229698
Ethan Fang <efang@deputy.com>
2014-01-06 09:28:39 +1100

    Refactoring structure

---
+++ DPCalendar/Source/Monthly/DPCalendarMonthlyMainView.m
@@ -0,0 +1,31 @@
+//
+//  DPCalendarMonthlyMainView.m
+//  DPCalendar
+//
+//  Created by Ethan Fang on 6/01/14.
+//  Copyright (c) 2014 Ethan Fang. All rights reserved.
+//
+
+#import "DPCalendarMonthlyMainView.h"
+
+@implementation DPCalendarMonthlyMainView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
